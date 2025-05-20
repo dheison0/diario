537b43260a72f3cdb4ff6b6cf0e3842ecc3a0817
Dheison Gomes <dheisomgomes@gmail.com>
2025-05-19 22:46:38 -0300

    Extração de todas as informações necessárias do Diário Oficial dos Municipios concluida

---
+++ app/diario.js
@@ -0,0 +1,142 @@
+import { MIN_RESULTS_PER_PAGE, WEBSITE_URL } from './config.js'
+
+export class Diario {
+    /**
+     * Initializes a new instance of the Diario class.
+     *
+     * @param {object} browser - An instance of the browser to be used for page navigation and interactions.
+     */
+    constructor(browser) {
+        this.browser = browser
+        this.iframeSelector = 'iframe[id="nmsc_iframe_ConPublicacaoGeral"]:not([src=""])'
+    }
+
+    /**
+     * Opens a new page and navigates to the website for the Diario dos
+     * Municipios.
+     *
+     * @throws {Error} If the Diario was already opened.
+     *
+     * @returns {Promise<void>}
+     */
+    async open() {
+        if (this.page !== undefined) {
+            throw "Diario already opened"
+        }
+        this.page = await this.browser.newPage()
+        await this.page.goto(WEBSITE_URL)
+        await this.page.waitForSelector("#SC_data_cond", { state: "hidden" })
+    }
+
+    async _extractOptions(query) {
+        return await this.page.evaluate(
+            q => Array.from(
+                document.querySelectorAll(q),
+                i => ({ name: i.innerText, value: i.value })
+            ),
+            query + ' > option'
+        )
+    }
+
+    /**
+     * Retrieves the available options for editions, entities, and cities
+     * from the webpage.
+     *
+     * @returns {Promise<Object>} An object containing arrays of options for
+     *                           editions, entities, and cities.
+     */
+    async getOptions() {
+        return {
+            'editions': await this._extractOptions('#SC_numedicao'),
+            'entities': await this._extractOptions('#SC_nomeentidade'),
+            'cities': await this._extractOptions('#SC_nomemunicipio'),
+        }
+    }
+
+    /**
+     * Fills the form on the webpage with the given options.
+     * 
+     * @param {{edition: {value: string}, city: {value: string}, entity: {value: string}}} options - The options to be filled in the form.
+     * 
+     * @returns {Promise<void>}
+     */
+    async fillForm({ edition, city, entity }) {
+        await this.page.selectOption('select[name="numedicao"]', edition.value)
+        await this.page.selectOption('select[name="nomeentidade"]', entity.value)
+        await this.page.selectOption('select[name="nomemunicipio"]', city.value)
+    }
+
+
+    async getResults() {
+        // Carrega os primeiros resultados
+        await this.page.click('a#sc_b_pesq_bot')
+        await this.page.waitForSelector(this.iframeSelector, { state: "visible" })
+        await this.page.waitForLoadState('networkidle')
+
+        const loadSearchResults = (iframeSelector) => {
+            const rows = document.querySelector(iframeSelector)
+                .contentDocument
+                .querySelectorAll("tr.scGridFieldEven, tr.scGridFieldOdd")
+            return Array.from(rows, i => {
+                const cells = i.querySelectorAll("td")
+                return {
+                    "id": cells[cells.length - 1].innerText.trim(),
+                    'edition': cells[1].innerText.trim(),
+                    'date': cells[3].innerText.trim(),
+                    'category': cells[6].innerText.trim(),
+                    'document': cells[7].innerText.trim(),
+                    'file': cells[8].querySelector('a').href.split("'")[1].trim()
+                }
+            })
+        }
+
+        const result = {}
+        while (true) {
+            const newResults = await this.page.evaluate(loadSearchResults, this.iframeSelector)
+            newResults.forEach(i => { result[i.id] = i })
+            if (newResults.length < MIN_RESULTS_PER_PAGE) {
+                break
+            }
+            // FIXME: Essa função ainda retorna false as vezes mesmo já estando na
+            //        última página de resultados.
+            // Essa função vai tentar indicar se já chegou ao fim da busca
+            const stop = await this.page.evaluate(
+                (i) => {
+                    const f = document.querySelector(i).contentDocument
+                    if (f.querySelector('img#id_img_forward_bot').src.includes('off')) {
+                        return true
+                    }
+                    f.querySelector('a#forward_bot').click()
+                    return false
+                },
+                this.iframeSelector
+            )
+            if (stop) {
+                break
+            }
+            await this.page.waitForLoadState('networkidle')
+            await this.page.waitForTimeout(250)
+        }
+        return Object.values(result)
+    }
+
+    /**
+     * Reloads the current page.
+     * 
+     * @returns {Promise<void>}
+     */
+    async reload() {
+        await this.page.reload()
+    }
+
+    /**
+     * Closes the current page if it is open.
+     *
+     * @returns {Promise<void>}
+     */
+    async close() {
+        if (this.page === undefined) return
+        await this.page.close()
+        this.page = undefined
+    }
+}
--- app/index.js
+++ app/index.js
@@ -1,98 +1,27 @@
 import playwright from 'playwright'
-import { MIN_RESULTS_PER_PAGE, WEBSITE_URL } from './config.js'
-
-const iframeSelector = 'iframe[id="nmsc_iframe_ConPublicacaoGeral"]:not([src=""])'
-
-const extractOptions = async (page, query) => await page.evaluate(
-    q => Array.from(document.querySelectorAll(q)).map(i => ({ name: i.innerText.trim(), value: i.value })),
-    query
-)
-
-const findOption = (nameLike, options) => options.find(i => i.name.toLowerCase().match(nameLike.toLowerCase()))
-
-function loadSearchResults(iframeSelector) {
-    const rows = document.querySelector(iframeSelector).contentDocument.querySelectorAll("tr.scGridFieldEven, tr.scGridFieldOdd")
-    return Array.from(rows).map(i => {
-        const cells = i.querySelectorAll("td")
-        return {
-            "id": cells[cells.length - 1].innerText.trim(),
-            'edition': cells[1].innerText.trim(),
-            'date': cells[3].innerText.trim(),
-            'category': cells[6].innerText.trim(),
-            'document': cells[7].innerText.trim(),
-            'file': cells[8].querySelector('a').href.split("'")[1].trim()
-        }
-    })
-}
+import { Diario } from './diario.js'
+import { findOption, showOptions } from './utils.js'
 
 async function main() {
     // Inicializa a base das pesquisas
-    console.log("Abrindo navegador...")
     const browser = await playwright.chromium.launch()
-    console.log("Acessando site...")
-    const page = await browser.newPage()
-    await page.goto(WEBSITE_URL)
-    console.log("Esperando seletor da data aparecer...")
-    await page.waitForSelector("#SC_data_cond", { state: "hidden" })
+    const jurema = new Diario(browser)
+    await jurema.open()
+    const options = await jurema.getOptions()
 
-    // Obtem as informações básicas para pesquisa
-    const editions = await extractOptions(page, '#SC_numedicao > option')
-    const entities = await extractOptions(page, '#SC_nomeentidade > option')
-    const cities = await extractOptions(page, '#SC_nomemunicipio > option')
-
-    // Escolhe o local que deseja obter as informações
-    const city = findOption("Jurema", cities)
-    const entity = findOption("Prefeitura", entities)
-    const edition = findOption("5317", editions)
-
-    console.log("=".repeat(50))
-    console.log("Informações selecionadas:")
-    console.log(`  Cidade: "${city.name}"`)
-    console.log(`  Entidade: "${entity.name}"`)
-    console.log(`  Edição: "${edition.name}" (${edition.value})`)
-    console.log("=".repeat(50))
-
-    console.log("Selecionando dados a serem procurados...")
-    await page.selectOption('select[name="numedicao"]', edition.value)
-    await page.selectOption('select[name="nomeentidade"]', entity.value)
-    await page.selectOption('select[name="nomemunicipio"]', city.value)
-
-    console.log("Esperando por resultados...")
-    await page.click('a#sc_b_pesq_bot')
-    await page.waitForSelector(iframeSelector, { state: "visible" })
-    await page.waitForLoadState('networkidle')
-    console.log("Resultados obtidos!")
-
-    console.log("Carregando dados...")
-    const result = {}
-    while (true) {
-        const newResults = await page.evaluate(loadSearchResults, iframeSelector)
-        newResults.forEach(i => { result[i.id] = i })
-        if (newResults.length < MIN_RESULTS_PER_PAGE) {
-            break
-        }
-        // FIXME: Essa função ainda retorna false as vezes mesmo já estando na última página de reultados
-        const stop = await page.evaluate( // Essa função vai tentar indicar se já chegou ao fim da busca
-            (i) => {
-                const iframe = () => document.querySelector(i).contentDocument
-                const buttonDisabled = iframe().querySelector('img#id_img_forward_bot').src.includes('off')
-                if (buttonDisabled) {
-                    return true
-                }
-                iframe().querySelector('a#forward_bot').click()
-                return false
-            },
-            iframeSelector
-        )
-        if (stop) {
-            break
-        }
-        await page.waitForLoadState('networkidle')
-        await page.waitForTimeout(250)
+    const selectedOptions = {
+        city: findOption("Jurema", options.cities),
+        entity: findOption("Prefeitura", options.entities),
+        edition: findOption("5317", options.editions),
     }
 
-    console.log(result)
-    console.log("Fim")
+    showOptions(selectedOptions)
+
+    await jurema.fillForm(selectedOptions)
+    const results = await jurema.getResults()
+    console.log(results)
+
+    await jurema.close()
     await browser.close()
 }
 
